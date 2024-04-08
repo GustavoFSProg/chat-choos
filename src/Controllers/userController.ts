b46740be17163f3451015d67146074c9cb8fd958
GusFSProg <gustavo.prog40@gmail.com>
2024-04-08 12:00:16 -0300

    login

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import { Request, Response } from 'express'
 import md5 from 'md5'
+import { generateToken } from '../token'
 
 const prismaDB = new PrismaClient()
 
@@ -27,6 +28,48 @@ async function getUsers(req: Request, res: Response) {
     return res.status(201).json(data)
   } catch (error) {
     return res.status(400).json({ error, message: 'ERROR!!' })
+  }
+}
+
+async function getOneUser(req: Request, res: Response) {
+  try {
+    const data = await prismaDB.users.findFirst({
+      where: { id: req.params.id },
+    })
+
+    return res.status(201).json(data)
+  } catch (error) {
+    return res.status(400).json({ error, message: 'ERROR!!' })
+  }
+}
+
+async function Login(req: Request, res: Response) {
+  try {
+    const dados = {
+      email: req.body.email,
+      password: String(md5(req.body.password, process.env.SECRET as string & { asBytes: true })),
+    }
+
+    if (dados.email === '' || dados.password === '') {
+      return res.json({ msgError: 'Email ou Senha inválido!!' })
+    }
+
+    const login = await prismaDB.users.findFirst({
+      where: {
+        email: req.body.email,
+        password: String(md5(req.body.password, process.env.SECRET as string & { asBytes: true })),
+      },
+    })
+
+    if (!login) {
+      return res.status(200).send({ msg: 'ERRO no Login!!' })
+    }
+
+    const token = await generateToken(dados)
+
+    return res.status(201).send({ msg: 'Logado com sucesso!!', token })
+  } catch (error) {
+    return res.status(400).send({ msg: 'Error!', error })
   }
 }
 
@@ -59,4 +102,4 @@ async function deleteUser(req: Request, res: Response) {
   }
 }
 
-export default { createUser, getUsers, deleteUser, updateUser }
+export default { createUser, Login, getUsers, getOneUser, deleteUser, updateUser }
--- src/routes.ts
+++ src/routes.ts
@@ -7,9 +7,9 @@ routes.get('/', (req: Request, res: Response) => {
   return res.status(200).json(`🧠 Api Template Running`)
 })
 
-// aaa
-
 routes.get('/get-users', userController.getUsers)
+routes.get('/get-one-user/:id', userController.getOneUser)
+routes.post('/login', userController.Login)
 routes.post('/create-user', userController.createUser)
 routes.put('/update-user/:id', userController.updateUser)
 routes.delete('/delete-user/:id', userController.deleteUser)
