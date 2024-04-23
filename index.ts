427450aaebaf61940d5b4a6bc6295a7e216543a4
GusFSProg <gustavo.prog40@gmail.com>
2024-04-23 11:33:07 -0300

    nvoas vercel

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import routes from './src/routes'
+
+dotenv.config()
+
+const { PORT } = process.env
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`🧠 Api Template Running ${PORT}`)
+})
+
+export default app
